a2b476642051fdcc6ebe4d7d7fdac10006ad27e6
Somers Matthews <somersbmatthews@gmail.com>
2019-10-14 16:00:42 -0500

    first attempt at code mirror implementation

--- app/app.js
+++ app/app.js
@@ -3,10 +3,15 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import CodeMirror from 'codemirror'
+import registerElixirMode from 'codemirror-mode-elixir'
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
+
+  CodeMirror.fromTextArea(document.getElementById('code'), { mode: 'elixir' })
 }
 
 loadInitializers(App, config.modulePrefix);
