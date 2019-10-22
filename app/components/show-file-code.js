687a202e98cb656305fce1dcfc0ab4d3ea067799
Somers Matthews <somersbmatthews@gmail.com>
2019-10-22 10:50:22 -0500

    uninstalled npm codemirror and installed ember ace

--- app/components/show-file-code.js
+++ app/components/show-file-code.js
@@ -1,13 +1,21 @@
 import Component from '@glimmer/component';
-import CodeMirror from 'codemirror';
-import registerElixirMode from 'codemirror-mode-elixir';
+
+
+
+// import CodeMirror from 'codemirror';
+// import registerElixirMode from 'codemirror-mode-elixir';
+
+// export default class ShowFileCodeComponent extends Component {
+//    @action setupCodeMirror() {
+//       CodeMirror
+//          .fromTextArea(
+//             document.getElementById('code'),
+//             {mode: 'elixir'}
+//          )
+//    }
+// }
+
 
 export default class ShowFileCodeComponent extends Component {
-   @action setupCodeMirror() {
-      CodeMirror
-         .fromTextArea(
-            document.getElementById('code'),
-            {mode: 'elixir'}
-         )
-   }
+
 }
